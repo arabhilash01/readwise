64f9702195dafc69f0ee63830621d3c5f15fca76
Abilash A R <abilash.ar@netcracker.com>
2025-08-11 17:33:39 +0530

    home sreen layout

--- lib/presentation/explore/widgets/bookshelf.dart
+++ lib/presentation/explore/widgets/bookshelf.dart
@@ -10,7 +10,8 @@ class Bookshelf extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      padding: const EdgeInsets.all(8),
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         crossAxisSpacing: 8,
